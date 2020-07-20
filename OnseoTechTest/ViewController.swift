37557d71250554c1aaf22d5f05d08714fac94781
Yaroslav Vushnyak <y.vushnyak@indevlab.com>
2020-07-20 23:07:24 +0300

    Initial Commit

---
+++ OnseoTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnseoTechTest
+//
+//  Created by Yaroslav Vushnyak on 20.07.2020.
+//  Copyright © 2020 Yaroslav Vushnyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
